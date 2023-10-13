208ee74b0715825e007a5a1ee97ae26d7ad75335
nickmeyer17 <nick.c.meyer17@gmail.com>
2023-10-12 22:28:32 -0400

    CleanUp

--- PitchingTeamReport.r
+++ PitchingTeamReport.r
@@ -9,15 +9,18 @@ library(grid)
 library(gridExtra)
 library(googledrive)
 
-
-
-
 Cluster_LocalDirectory <- Sys.getenv("Cluster_LocalDirectory")
 Cluster_FieldName <- Sys.getenv("Cluster_FieldName")
 
-
 directory <- setwd(Cluster_LocalDirectory)
-date <- 20231008
+# Get the current system date
+current_date <- Sys.Date()
+# Determine the previous day's date
+previous_date <- current_date - 1
+# Format the previous date as YYYYMMDD
+date <- format(previous_date, "%Y%m%d")
+#date <- 20231008 #Use if not running for previous date
+
 file <- paste0(date,"-", Cluster_FieldName,"-Private-1_unverified.csv")
 df <- read.csv(file)
 str(df)
@@ -40,9 +43,7 @@ stats <- merge(strike_per, first_pitch, by = "Pitcher")
 df$successful_first_three <- logical(nrow(df))
 
 for(PA in unique(df$PAofGame)){
-   #sf <- df %>% filter(PAofGame == PA)
-   #sf$successful_first_three <- logical(nrow(sf))
-for (i in 1:nrow(df)) {
+  for (i in 1:nrow(df)) {
   if (df$strike_count[i] >= 2 && df$Balls[i] <= 1) {
     df$success[i] <- TRUE
   } else {
@@ -51,10 +52,7 @@ for (i in 1:nrow(df)) {
 }
 }
 sf <- df %>% select(Pitcher, PAofGame, PitchofPA, success, Balls, strike_count)
-sf
 sf <- sf %>% group_by(PAofGame) %>% filter(PitchofPA == max(PitchofPA[PitchofPA < 3 | PitchofPA == 3]))
-sf
-
 
 f3_success <- sf %>% group_by(Pitcher) %>% summarize(f_2of3_success = round(mean(success, na.rm = TRUE)*100, 1))
 stats <- merge(stats, f3_success, by = "Pitcher")
@@ -67,11 +65,7 @@ action_pitch <- filter(df, in_play %in% c("TRUE"))
 action <- action_pitch %>% mutate(on_base = ifelse(PlayResult %in% c("Single", "Double", "Triple", "HomeRun")| KorBB %in% c("Walk")| PitchCall %in% c("HitByPitch"), TRUE, FALSE))
 action <- action %>% select(Pitcher, PAofGame, on_base)
 
-str(action)
-str(sf)
-
 data <- merge(action, sf, by = c("Pitcher", "PAofGame"))
-str(data)
 data <- filter(data, success %in% c("TRUE"))
 
 f3_obp <- data %>% group_by(Pitcher) %>% summarize(
@@ -82,9 +76,7 @@ stats <- merge(stats, f3_obp, by = "Pitcher")
 
 output_filename <- paste0(date, "_PitchingKPIReport.pdf")
 
-
 pdf(output_filename, height = 8.5, width = 11)
 grid.table(stats)
 print(stats)
 dev.off()
-
