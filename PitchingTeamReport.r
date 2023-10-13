fd9ffd7c7ac7a6aba7d8839379af3a9cf031884d
nickmeyer17 <nick.c.meyer17@gmail.com>
2023-10-12 22:24:27 -0400

    Correct 1st 2 of 3 success calculations

--- PitchingTeamReport.r
+++ PitchingTeamReport.r
@@ -23,7 +23,7 @@ df <- read.csv(file)
 str(df)
 
 df <- filter(df, PitcherTeam %in% c("DAY_FLY"))
-df$PAofGame <- paste0(df$Inning, ".", df$PAofInning)
+df$PAofGame <- paste0(df$Inning, ".", df$PAofInning, ".", df$Top.Bottom)
 
 df <- df %>% mutate(is_strike = ifelse(PitchCall %in% c("StrikeCalled", "InPlay", "StrikeSwinging", "FoulBall"), 1, 0))
 df <- df %>% mutate(strike_count = Strikes + is_strike)
@@ -37,29 +37,41 @@ strike_per <- df %>% group_by(Pitcher) %>% summarize(strike_percent = round(mean
 first_pitch <- first_pitch %>% group_by(Pitcher) %>% summarize(first_pitch_strike = round(mean(is_strike, na.rm = 1)*100, 1))
 stats <- merge(strike_per, first_pitch, by = "Pitcher")
 
+df$successful_first_three <- logical(nrow(df))
 
-first_three <- filter(df, PitchofPA == 3)
-str(first_three)
-first_three <- first_three %>% mutate(success = ifelse(strike_count >= 2, TRUE, FALSE))
+for(PA in unique(df$PAofGame)){
+   #sf <- df %>% filter(PAofGame == PA)
+   #sf$successful_first_three <- logical(nrow(sf))
+for (i in 1:nrow(df)) {
+  if (df$strike_count[i] >= 2 && df$Balls[i] <= 1) {
+    df$success[i] <- TRUE
+  } else {
+    df$success[i] <- FALSE
+  }
+}
+}
+sf <- df %>% select(Pitcher, PAofGame, PitchofPA, success, Balls, strike_count)
+sf
+sf <- sf %>% group_by(PAofGame) %>% filter(PitchofPA == max(PitchofPA[PitchofPA < 3 | PitchofPA == 3]))
+sf
 
 
+f3_success <- sf %>% group_by(Pitcher) %>% summarize(f_2of3_success = round(mean(success, na.rm = TRUE)*100, 1))
+stats <- merge(stats, f3_success, by = "Pitcher")
 
-first_three_success <- first_three %>% group_by(Pitcher) %>% summarize(first_three_success = round(mean(success, na.rm = TRUE)*100, 1))
-stats <- merge(stats, first_three_success, by = "Pitcher")
+sf <- sf %>% select(Pitcher, PAofGame, success)
 
 df <- df %>% mutate(in_play = ifelse(PitchCall %in% c("InPlay", "HitByPitch") | KorBB %in% c("Walk", "Strikeout"), TRUE, FALSE))
 action_pitch <- filter(df, in_play %in% c("TRUE"))
 
 action <- action_pitch %>% mutate(on_base = ifelse(PlayResult %in% c("Single", "Double", "Triple", "HomeRun")| KorBB %in% c("Walk")| PitchCall %in% c("HitByPitch"), TRUE, FALSE))
-action <- filter(action, PitchofPA >= 3)
 action <- action %>% select(Pitcher, PAofGame, on_base)
 
-f3 <- first_three %>% select(Pitcher, PAofGame, success)
 str(action)
-str(f3)
-data <- merge(action, f3, by = c("Pitcher", "PAofGame"))
-str(data)
+str(sf)
 
+data <- merge(action, sf, by = c("Pitcher", "PAofGame"))
+str(data)
 data <- filter(data, success %in% c("TRUE"))
 
 f3_obp <- data %>% group_by(Pitcher) %>% summarize(
@@ -75,3 +87,4 @@ pdf(output_filename, height = 8.5, width = 11)
 grid.table(stats)
 print(stats)
 dev.off()
+
